1d260a8a456101bf53c83741f98ddc55f45c92f9
Joeri <joeri.dijkstra@bettyblocks.com>
2024-05-18 12:02:23 +0200

    fix: reset password styling

--- lib/dragonhoard_web/live/user_reset_password_live.ex
+++ lib/dragonhoard_web/live/user_reset_password_live.ex
@@ -29,11 +29,6 @@ defmodule DragonhoardWeb.UserResetPasswordLive do
           <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
         </:actions>
       </.simple_form>
-
-      <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
-      </p>
     </div>
     """
   end
