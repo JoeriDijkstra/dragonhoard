b7c328a3dabe5d5d55c65a1863bc42657474104a
Joeri <joeri.dijkstra@bettyblocks.com>
2024-05-18 12:16:28 +0200

    feat: official release!

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dragonhoard.MixProject do
   def project do
     [
       app: :dragonhoard,
-      version: "0.1.1",
+      version: "1.0.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
