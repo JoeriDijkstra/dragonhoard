4fbae503a0f4a374e6495d7f13a9d4f423da9661
Joeri Dijkstra <joeridijkstra@Joeris-MacBook-Air.local>
2024-10-01 17:02:34 +0200

    fix: ensure script load order

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dragonhoard.MixProject do
   def project do
     [
       app: :dragonhoard,
-      version: "1.2.1",
+      version: "1.2.3",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
